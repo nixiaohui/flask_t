273a25788b231c7e5e0f61cce8bea3a73876ed05
Hugo <6089870@qq.com>
2019-06-24 18:55:59 +0800

    修改静态资源的引用

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="statics")
 
 @app.route("/")
 def index():
     return "flask working now..."
+
+@app.route("/desk")
+def desk():
+    return render_template("desk.html")
 
 def main():
     app.run(debug=True, host="0.0.0.0")
